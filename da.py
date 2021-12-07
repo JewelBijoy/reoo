574e310cb0adcb474e65b416e0dbc18759d21f6a
Jewel Bijoy(UST <187260@ust-global.com>
2021-12-07 08:40:34 +0530

    first

---
+++ da.py
@@ -0,0 +1,12 @@
+from py2neo import Graph,Node,Relationship
+from flask import Flask,render_template,request
+import json
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+   
+if __name__=='__main__':
+    app.run(debug=True)
